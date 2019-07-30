2784980deabb7e7455e1ea69a4324e6013044f86
ramises <ramises.martins@intelie.com.br>
2019-07-30 10:04:13 -0300

    Improved example

--- example/example.py
+++ example/example.py
@@ -1,8 +1,6 @@
-import cometd_live_client
-from tcp_live_client import TcpClient
+from liveclient import LiveClient, Query, AbstractListener
 
-
-class PrintListener(cometd_live_client.AbstractListener):
+class PrintListener(AbstractListener):
     def __init__(self):
         super().__init__()
 
@@ -10,23 +8,23 @@ class PrintListener(cometd_live_client.AbstractListener):
         print(event)
 
 
-class SendToLiveListener(cometd_live_client.AbstractListener):
-    def __init__(self, ip, port):
+class SendToLiveListener(AbstractListener):
+    def __init__(self, tcp_client):
         super().__init__()
-        self.tcp_client = TcpClient('localhost', 17042)
+        self.tcp_client = tcp_client
 
     def on_event(self, event):
         self.tcp_client.send(event)
 
+live_client = LiveClient("config.json")
 
-query1 = cometd_live_client.Query("=> random() as r1 every second")
+query1 = Query("=> random() as r1 every second")
 query1.add_listener(PrintListener())
-query1.add_listener(SendToLiveListener('localhost', 17042))
+query1.add_listener(SendToLiveListener(live_client.tcp_client))
 
-query2 = cometd_live_client.Query("=> random() as r2 every second")
+query2 = Query("=> random() as r2 every second")
 query2.add_listener(PrintListener())
 
-client = cometd_live_client.CometdClient(
-    "http://localhost:8080", "admin", "admin", [query1, query2])
-
-client.execute()
+cometd_live_client = live_client.cometd_client
+cometd_live_client.add_queries([query1, query2])
+cometd_live_client.execute()
